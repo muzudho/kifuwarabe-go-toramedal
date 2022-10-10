c0ec18bc0b77ca57602afad6d86f2b9a1c16e664
むずでょ <muzudho1@gmail.com>
2022-10-10 20:13:22 +0900

    put stone

--- engine/kernel/o20_put_stone_no_record.go
+++ engine/kernel/o20_put_stone_no_record.go
@@ -1,24 +1,5 @@
 package kernel
 
-import (
-	"os"
-
-	code "github.com/muzudho/kifuwarabe-go-toramedal/engine/coding_obj"
-)
-
-// PutStoneOnRecord - SelfPlay, RunGtpEngine から呼び出されます
-func PutStoneOnRecord(position *Position, z Point, color Stone, recItem *RecordItem) {
-	var err = PutStone(position, z, color)
-	if err != 0 {
-		code.Console.Error("(PutStoneOnRecord) Err!\n")
-		os.Exit(0)
-	}
-
-	// 棋譜に記録
-	position.Record[position.MovesNum] = recItem
-	position.MovesNum++
-}
-
 // PutStone - 石を置きます。
 // * `z` - 交点。壁有り盤の配列インデックス
 //
